46d8048698ded517c150da2da7135bf09d959d2a
Isur <bednarczykartur96@gmail.com>
2024-01-27 14:31:44 +0100

    Ansbile view instead of decrypt and encrypt

--- install/actions/repos.sh
+++ install/actions/repos.sh
@@ -1,19 +1,18 @@
 work_repos () {
-	ansible-vault decrypt ./actions/work_repos.txt --vault-password-file $HOME/dotfiles/.vault_pass
-	while IFS= read -r line; do
+	work=$(ansible-vault view ./actions/work_repos.txt --vault-password-file $HOME/dotfiles/.vault_pass)
+
+	while read -r line; do
 		arr=($line)
 		mkdir -p $HOME/Developer/Clients/${arr[0]}
 		(cd $HOME/Developer/Clients/${arr[0]} && git clone ${arr[1]})
-	done < $HOME/dotfiles/install/actions/work_repos.txt
-	ansible-vault encrypt ./actions/work_repos.txt --vault-password-file $HOME/dotfiles/.vault_pass
+	done <<< "$work"
 }
 
 private_repos () {
-	ansible-vault decrypt ./actions/private_repos.txt --vault-password-file $HOME/dotfiles/.vault_pass
-	while IFS= read -r line; do
+	private=$(ansible-vault view ./actions/private_repos.txt --vault-password-file $HOME/dotfiles/.vault_pass)
+
+	while read -r line; do
 		arr=($line)
 		(cd $HOME/Developer/Personal && git clone ${arr[0]})
-	done < $HOME/dotfiles/install/actions/private_repos.txt
-	ansible-vault encrypt ./actions/private_repos.txt --vault-password-file $HOME/dotfiles/.vault_pass
+	done <<< "$private"
 }
-
