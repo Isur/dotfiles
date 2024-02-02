f3e83ead8ee63a12805cae93f0695c3370e9b2e6
Isur <bednarczykartur96@gmail.com>
2024-02-02 23:15:33 +0100

    i3 config for ubuntu

--- install/actions/tiling.sh
+++ install/actions/tiling.sh
@@ -3,7 +3,19 @@ arch_tiling () {
 }
 
 debian_tiling () {
-	echo
+	/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+	sudo apt install ./keyring.deb
+	echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+	sudo apt update
+	sudo apt install i3 -y
+	rm ./keyring.deb
+
+	sudo apt install rofi -y
+	sudo apt install feh -y
+
+	create_symlink "i3" "i3" ".config/i3"
+	create_symlink "i3status" "i3status" ".config/i3status"
+	create_symlink "rofi" "rofi" ".config/rofi"
 }
 
 macos_tiling () {
