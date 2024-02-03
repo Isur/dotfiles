430abcebcd858fa7284635c57180b1b19bcd44fa
Isur <bednarczykartur96@gmail.com>
2024-02-03 17:42:48 +0100

    BetterlockScreen dunst

--- install/actions/tiling.sh
+++ install/actions/tiling.sh
@@ -1,9 +1,11 @@
 arch_tiling () {
-	yay -S i3-wm rofi feh i3status --noconfirm
+	yay -S i3-wm rofi feh i3status dunst betterlockscreen-git --noconfirm
 
 	create_symlink "i3" "i3" ".config/i3"
 	create_symlink "i3status" "i3status" ".config/i3status"
 	create_symlink "rofi" "rofi" ".config/rofi"
+	create_symlink "dunst" "dunst" ".config/dunst"
+	create_symlink "betterlockscreen" "betterlockscreen" ".config/betterlockscreen"
 }
 
 debian_tiling () {
@@ -16,10 +18,13 @@ debian_tiling () {
 
 	sudo apt install rofi -y
 	sudo apt install feh -y
+	sudo apt install betterlockscreen -y
 
 	create_symlink "i3" "i3" ".config/i3"
 	create_symlink "i3status" "i3status" ".config/i3status"
 	create_symlink "rofi" "rofi" ".config/rofi"
+	create_symlink "dunst" "dunst" ".config/dunst"
+	create_symlink "betterlockscreen" "betterlockscreen" ".config/betterlockscreen"
 }
 
 macos_tiling () {
