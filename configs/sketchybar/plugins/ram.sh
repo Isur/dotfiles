69f04d412604673d483ae4a9cffc83362d38c21b
Isur <bednarczykartur96@gmail.com>
2024-03-06 20:57:08 +0100

    Tiling removed from macos
    It not work as nice as on linux.

--- configs/sketchybar/plugins/ram.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-RAM_PERCENT="$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f\n", 100-$5"%") }')%"
-
-sketchybar --set $NAME label="$RAM_PERCENT"
--- install/actions/tiling.sh
+++ install/actions/tiling.sh
@@ -33,18 +33,7 @@ debian_tiling () {
 }
 
 macos_tiling () {
-	brew install koekeishiya/formulae/yabai
-	brew install koekeishiya/formulae/skhd
-	brew tap FelixKratz/formulae
-	brew install sketchybar
-	brew install jq
-	curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.16/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-	curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.20/icon_map_fn.sh -o ~/.config/sketchybar/plugins/icon_map_fn.sh
 	brew install --cask raycast
-
-	create_symlink "yabai" "yabai" ".config/yabai"
-	create_symlink "skhd" "skhd" ".config/skhd"
-	create_symlink "sketchybar" "sketchybar" ".config/sketchybar"
 }
 
 install_tiling () {
