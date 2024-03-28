583efd0a83ac46f118dec1d23bb6f020e130d2cf
Isur <bednarczykartur96@gmail.com>
2024-03-28 22:59:36 +0100

    cmp confirm new keybind

--- configs/nvim/lua/isur/plugins/code/nvim-cmp.lua
+++ configs/nvim/lua/isur/plugins/code/nvim-cmp.lua
@@ -33,17 +33,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.close(),
-				["<CR>"] = cmp.mapping({
-					i = function(fallback)
-						if cmp.visible() and cmp.get_active_entry() then
-							cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-						else
-							fallback()
-						end
-					end,
-					s = cmp.mapping.confirm({ select = true }),
-					c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-				}),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 				["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
 				["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
 			}),
