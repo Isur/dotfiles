d5408c980b607f4328f576f789f482e747c9e859
Isur <bednarczykartur96@gmail.com>
2024-04-03 21:22:10 +0200

    Key mapping for removing search highlights

--- configs/nvim/lua/isur/mappings.lua
+++ configs/nvim/lua/isur/mappings.lua
@@ -10,3 +10,6 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Remove highlights
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
