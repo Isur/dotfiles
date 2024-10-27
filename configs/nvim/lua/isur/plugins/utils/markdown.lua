3a3b21f463c05a7d8368a661c6956862a24e1ee4
Isur <bednarczykartur96@gmail.com>
2024-10-27 12:17:46 +0100

    markdown

--- configs/nvim/lua/isur/plugins/utils/markdown.lua
+++ configs/nvim/lua/isur/plugins/utils/markdown.lua
@@ -1,8 +1,9 @@
 return {
 	{
-		"iamcco/markdown-preview.nvim",
-		config = function()
-			vim.fn["mkdp#util#install"]()
-		end,
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
 	},
 }
