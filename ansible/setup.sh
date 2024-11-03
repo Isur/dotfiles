2a4b750fdac91a5d4ec14920de7ebcb2554524ec
Isur <bednarczykartur96@gmail.com>
2024-11-03 09:48:33 +0100

    Ansible: galaxy in script

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/Isur/dotfiles ~/dotfiles
 yay -S ansible --noconfirm
-(cd ~/dotfiles && ansible-playbook -i inventory.yaml play.yaml -K)
+(cd ~/dotfiles && ansible-galaxy install -r collections.yaml && ansible-playbook -i inventory.yaml play.yaml -K)
 
