b78b25e3cbbe6177a5c20ae9ae375f9faf1e8beb
Isur <bednarczykartur96@gmail.com>
2024-11-10 23:14:28 +0100

    test

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -9,7 +9,8 @@ BRANCH=ansible
 
 git clone $REPO_URL $DOTFILES
 
-(cd $ANSIBLE_DIR &&\
+(cd $DOTFILES &&\
     git checkout $BRANCH &&\
+    cd $ANSIBLE_DIR &&\
     ansible-galaxy install -r collections.yml &&\
     ansible-playbook play.yml -i inventory.yml -K --vault-password-file $HOME/.vault_pass)
