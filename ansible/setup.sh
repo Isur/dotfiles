28fa43237f95beee8befdacaf1f47846a2c50915
Isur <bednarczykartur96@gmail.com>
2024-11-10 23:12:15 +0100

    fix script

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -5,8 +5,11 @@ yay -S git ansible --noconfirm
 REPO_URL="https://github.com/Isur/dotfiles.git"
 DOTFILES="$HOME/dotfiles"
 ANSIBLE_DIR="$HOME/dotfiles/ansible"
+BRANCH=ansible
 
 git clone $REPO_URL $DOTFILES
 
-(cd $ANSIBLE_DIR && ansible-galaxy install -r collections.yml)
-(cd $ANSIBLE_DIR && ansible-playbook play.yml -i inventory.yml -K --vault-password-file $HOME/.vault_pass)
+(cd $ANSIBLE_DIR &&\
+    git checkout $BRANCH &&\
+    ansible-galaxy install -r collections.yml &&\
+    ansible-playbook play.yml -i inventory.yml -K --vault-password-file $HOME/.vault_pass)
