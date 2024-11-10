377062afb6b67bb99607337593550ba4fcbf3c0e
Isur <bednarczykartur96@gmail.com>
2024-11-10 23:04:30 +0100

    test

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -5,14 +5,7 @@ DOTFILES="$HOME/dotfiles"
 ANSIBLE_DIR="$HOME/dotfiles/ansible"
 REQUIREMENTS_FILE="$ANSIBLE_DIR/collections.yaml"
 
-# Pull the latest code
-ansible-pull -U "$REPO_URL" -d "$DOTFILES"
+git clone $REPO_URL $DOTFILES
 
-# Install collections specified in requirements.yml
-if [ -f "$REQUIREMENTS_FILE" ]; then
-    ansible-galaxy collection install -r "$REQUIREMENTS_FILE" -p "$ANSIBLE_DIR/collections"
-fi
-
-# Run ansible-pull with the environment variable for collections
-export ANSIBLE_COLLECTIONS_PATHS="$ANSIBLE_DIR/collections"
-ansible-pull -U "$REPO_URL" -d "$ANSIBLE_DIR" -i play.yml --vault-password-file $HOME/.vault_pass
+(cd $ANSIBLE_DIR && ansible-galaxy install -r collections.yaml)
+(cd $ANSIBLE_DIR && ansible-playbook play.yaml -i inventory.yaml -K --vault-password-file $HOME/.vault_pass)
