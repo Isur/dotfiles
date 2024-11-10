14a11dca04b437f78536e72f95e013ccd159352e
Isur <bednarczykartur96@gmail.com>
2024-11-10 23:10:02 +0100

    typo

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -9,4 +9,4 @@ ANSIBLE_DIR="$HOME/dotfiles/ansible"
 git clone $REPO_URL $DOTFILES
 
 (cd $ANSIBLE_DIR && ansible-galaxy install -r collections.yml)
-(cd $ANSIBLE_DIR && ansible-playbook play.yaml -i inventory.yml -K --vault-password-file $HOME/.vault_pass)
+(cd $ANSIBLE_DIR && ansible-playbook play.yml -i inventory.yml -K --vault-password-file $HOME/.vault_pass)
