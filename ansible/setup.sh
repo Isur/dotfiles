c10caaf00a996268b302b366a7cf9df1055e2a98
Isur <bednarczykartur96@gmail.com>
2024-11-10 22:30:55 +0100

    fix path

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -4,4 +4,4 @@ yay -S git ansible --noconfirm
 
 git clone https://github.com/Isur/dotfiles ~/dotfiles
 
-(cd ~/dotfiles && git checkout ansible && ansible-galaxy install -r collections.yaml && ansible-playbook play.yaml -i inventory.yaml -K --vault-password-file=$HOME/.vault_pass)
+(cd ~/dotfiles && git checkout ansible && cd ~/dotfiles/ansible && ansible-galaxy install -r collections.yaml && ansible-playbook play.yaml -i inventory.yaml -K --vault-password-file=$HOME/.vault_pass)
