e837970b94891d9dac6cf7200e3998921e781db4
Isur <bednarczykartur96@gmail.com>
2024-11-11 13:42:51 +0100

    Setup script

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,12 +1,53 @@
 #!/bin/bash
 
-yay -S git ansible --noconfirm
-
 REPO_URL="https://github.com/Isur/dotfiles.git"
 DOTFILES="$HOME/dotfiles"
 ANSIBLE_DIR="$HOME/dotfiles/ansible"
 BRANCH=ansible
+SYSTEM=""
 
+if [ "$(uname)" == "Darwin" ]; then
+	SYSTEM="Darwin"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	if [ -f /etc/arch-release ]; then
+		SYSTEM="Arch"
+	fi
+fi
+
+if [ "$SYSTEM" == "" ]; then
+    echo "System not supported. Script will work only on Arch based system or MacOS (Darwin)"
+	exit 1
+fi
+
+
+if [ "$SYSTEM" == "Arch" ]; then
+	if ! command -v yay &> /dev/null
+	then
+		sudo pacman -S --needed git base-devel --noconfirm
+		rm -rf yay
+		git clone https://aur.archlinux.org/yay.git
+		cd yay
+		makepkg -si
+		cd ..
+		rm -rf yay
+	fi
+
+	yay -S --noconfirm ansible python-psycopg2
+fi
+
+if [ "$SYSTEM" == "Darwin" ]; then
+	which -s brew
+	if [[ $? != 0 ]] ; then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	else
+		brew update
+	fi
+
+	brew install ansible
+	brew install postgresql
+fi
+
+rm -rf $DOTFILES
 git clone $REPO_URL $DOTFILES
 
 (cd $DOTFILES &&\
@@ -14,3 +55,4 @@ git clone $REPO_URL $DOTFILES
     cd $ANSIBLE_DIR &&\
     ansible-galaxy install -r collections.yml &&\
     ansible-playbook play.yml -i inventory.yml -K --vault-password-file $HOME/.vault_pass)
+
