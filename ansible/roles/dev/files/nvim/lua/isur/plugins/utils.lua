87442be557635c094b746d38615c147692b55fec
Isur <bednarczykartur96@gmail.com>
2025-01-24 20:23:20 +0100

    nvim: typr and undotree

--- ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
@@ -53,6 +53,15 @@ return {
 			},
 		},
 	},
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		event = { "BufReadPost" },
+		config = true,
+		keys = { -- load the plugin only when using it's keybinding:
+			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		},
+	},
 	-- {
 	-- 	"github/copilot.vim",
 	-- 	depenencies = { "ofseed/lualine-copilot" },
