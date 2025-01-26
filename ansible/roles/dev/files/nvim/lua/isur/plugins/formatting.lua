e36fcb66d1cb3f22ec2970562c52293a29c302f0
Isur <bednarczykartur96@gmail.com>
2025-01-26 14:58:25 +0100

    nvim: linter off env variable

--- ansible/roles/dev/files/nvim/lua/isur/plugins/formatting.lua
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/formatting.lua
@@ -16,13 +16,20 @@ return {
 
 			local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-			-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			-- 	group = lint_autogroup,
-			-- 	callback = function()
-			-- 		lint.try_lint()
-			-- 	end,
-			-- })
+			if vim.env.LINTER ~= "off" then
+				vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+					group = lint_autogroup,
+					callback = function()
+						print("Linting")
+						lint.try_lint()
+					end,
+				})
+			end
 
+			local buf = vim.api.nvim_get_current_buf()
+			vim.api.nvim_buf_create_user_command(buf, "Lint", function()
+				lint.try_lint()
+			end, {})
 			vim.keymap.set("n", "<leader>ml", function()
 				lint.try_lint()
 			end, { desc = "Trigger linting for current file" })
