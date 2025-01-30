8a90e62745115f28efa1fe2ed49b0be61780e154
Isur <bednarczykartur96@gmail.com>
2025-01-30 14:45:47 +0100

    nvim: spectre on macos fix

--- ansible/roles/dev/files/nvim/lua/isur/plugins/nvim-spectre.lua
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/nvim-spectre.lua
@@ -4,6 +4,23 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
+		if vim.loop.os_uname().sysname == "Darwin" then
+			-- This will fix problem with MacOS creating -E backup files
+			-- https://github.com/nvim-pack/nvim-spectre/issues/118
+			require("spectre").setup({
+				replace_engine = {
+					["sed"] = {
+						cmd = "sed",
+						args = {
+							"-i",
+							"",
+							"-E",
+						},
+					},
+				},
+			})
+		end
+
 		vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
 			desc = "Toggle Spectre",
 		})
