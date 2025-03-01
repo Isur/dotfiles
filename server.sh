6d2b3034ccf726abcc2ddf6547418532e7359365
Isur <bednarczykartur96@gmail.com>
2025-03-01 21:15:19 +0100

    feat: vim mode for zsh

--- server.sh
+++ server.sh
@@ -16,6 +16,8 @@ echo 'export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="bira"
 plugins=(zsh-fzf-history-search zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
+bindkey -v
+export KEYTIMEOUT=1
 export EDITOR="vim"
 ' >> $HOME/.zshrc
 
