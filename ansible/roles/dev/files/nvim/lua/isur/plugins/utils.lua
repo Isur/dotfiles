05bcab766ca55bc37e8030e5dca1fd88bdfb51d0
Isur <bednarczykartur96@gmail.com>
2025-04-25 19:25:31 +0200

    nvim: noice settings

--- ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
@@ -76,9 +76,6 @@ return {
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
-		opts = {
-			-- add any options here
-		},
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			"MunifTanjim/nui.nvim",
@@ -87,6 +84,26 @@ return {
 			--   If not available, we use `mini` as the fallback
 			"rcarriga/nvim-notify",
 		},
+		config = function()
+			local noice = require("noice")
+
+			noice.setup({
+				lsp = {
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+				},
+				presets = {
+					bottom_search = true, -- use a classic bottom cmdline for search
+					command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = false, -- enables an input dialog for inc-rename.nvim
+					lsp_doc_border = true, -- add a border to hover docs and signature help
+				},
+			})
+		end,
 	},
 	-- {
 	-- 	"github/copilot.vim",
