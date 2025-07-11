0239f957790161cfa9ce5a5268ce301659450038
Isur <bednarczykartur96@gmail.com>
2025-07-11 20:37:46 +0200

    nvim: nvim-biscuits

--- ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
@@ -145,4 +145,15 @@ return {
 	-- 		vim.g.copilot_no_tab_map = true
 	-- 	end,
 	-- },
+	{
+		"code-biscuits/nvim-biscuits",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("nvim-biscuits").setup({
+				cursor_line_only = true,
+			})
+		end,
+	},
 }
