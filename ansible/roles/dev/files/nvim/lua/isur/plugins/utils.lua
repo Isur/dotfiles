e1cf73140fde7ea467f610ef92fefa9082087e25
Isur <bednarczykartur96@gmail.com>
2025-08-01 22:35:54 +0200

    hypr: themes

--- ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
@@ -2,33 +2,12 @@ return {
 	"tpope/vim-sleuth",
 	"christoomey/vim-tmux-navigator",
 	{
-		"ellisonleao/gruvbox.nvim",
+		"folke/tokyonight.nvim",
+		lazy = false,
 		priority = 1000,
+		opts = {},
 		config = function()
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "hard", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = false,
-			})
-			vim.cmd("colorscheme gruvbox")
+			vim.cmd("colorscheme tokyonight-night")
 		end,
 	},
 	{
