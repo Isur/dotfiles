fa8149382f05c5f4bc6d0872811e7b4b3576b629
Isur <bednarczykartur96@gmail.com>
2025-09-01 21:55:09 +0200

    nvim: harpoon

---
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/harpoon.lua
@@ -0,0 +1,42 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+
+	config = function()
+		local harpoon = require("harpoon")
+
+		harpoon:setup({
+			settings = {
+				save_on_toggle = true,
+			},
+		})
+
+		vim.keymap.set("n", "<leader>hh", function()
+			harpoon:list():add()
+		end)
+		vim.keymap.set("n", "<leader>h", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end)
+
+		vim.keymap.set("n", "<leader><S-Tab>", function()
+			harpoon:list():prev({ ui_nav_wrap = true })
+		end)
+		vim.keymap.set("n", "<leader><Tab>", function()
+			harpoon:list():next({ ui_nav_wrap = true })
+		end)
+	end,
+}
