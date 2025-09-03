b263d71f0297fffba3473742d850a8a4fb675a92
Isur <bednarczykartur96@gmail.com>
2025-09-03 21:10:44 +0200

    theme: catppuccin-macchiato

--- ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
+++ ansible/roles/dev/files/nvim/lua/isur/plugins/utils.lua
@@ -2,11 +2,12 @@ return {
 	"tpope/vim-sleuth",
 	"christoomey/vim-tmux-navigator",
 	{
-		"Tsuzat/NeoSolarized.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 		config = function()
-			vim.cmd("colorscheme NeoSolarized")
+			require("catppuccin").setup()
+			vim.cmd("colorscheme catppuccin-macchiato")
 		end,
 	},
 	{
