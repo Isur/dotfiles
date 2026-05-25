9e068a0fec373c4fe495a88bd279a07f01aef9bb
Isur <bednarczykartur96@gmail.com>
2026-05-26 00:13:33 +0200

    nvim: animations

--- ansible/roles/general/files/nvim/lua/isur/plugins/utils.lua
+++ ansible/roles/general/files/nvim/lua/isur/plugins/utils.lua
@@ -10,4 +10,22 @@ return {
 			keymap.lazy("<leader>mm", ":MaximizerToggle<CR>", { desc = "UI: [m]aximizer [m]ode" }),
 		},
 	},
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			stiffness = 0.8,
+			trailing_stiffness = 0.6,
+			stiffness_insert_mode = 0.7,
+			trailing_stiffness_insert_mode = 0.7,
+			damping = 0.95,
+			damping_insert_mode = 0.95,
+			distance_stop_animating = 0.5,
+		},
+	},
+	{
+		"karb94/neoscroll.nvim",
+		opts = {
+			duration_multiplier = 0.3,
+		},
+	},
 }
